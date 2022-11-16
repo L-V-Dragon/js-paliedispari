f52e515e8560328ca89002fd67ebf7d5e8f97111
L-V-Dragon <viotti37@gmail.com>
2022-11-16 16:25:07 +0100

    Es completato

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// --------------- ES Pari Dispari ---------------
+
 let parolaUtente = prompt("Inserisci una parola");
 
 let parolaLowerCase = parolaUtente.toLowerCase();
@@ -17,4 +19,51 @@ function palilap(string){
         }
     }
     return true;
+}
+
+
+// --------------- ES Pari Dispari ---------------
+
+let scelta = prompt("Pari o Dispari?").toLowerCase();
+
+while(scelta !== "pari" && scelta !== "dispari"){
+    alert ("Ti ho detto che puoi scegliere solo tra pari e dispari, lurido analfabeta");
+    scelta = prompt("Pari o Dispari?").toLowerCase();
+}
+
+
+let numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
+
+while(numeroUtente > 5 || numeroUtente < 1){
+    alert ("Ci risiamo maledetta scimmia, puoi scegliere solo un numero da 1 a 5");
+    numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
+}
+
+let numeroBanco = randomBanco(1,5);
+console.log(numeroBanco);
+
+let somma = numeroUtente + numeroBanco;
+
+if(check(somma) == "pari" && scelta == "pari"){
+    console.log("Hai vinto!!");
+} else if (check(somma) == "dispari" && scelta == "dispari") {
+    console.log("Hai vinto!!");
+} else{
+    console.log("Hai perso!!");
+}
+
+function randomBanco(min, max){
+    let numeroCasuale = Math.random()* (max - min) + min;
+    return Math.round(numeroCasuale);
+}
+
+
+function check(num){
+    if (num % 2 == 0){
+        console.log("pari");
+        return "pari";
+    }else{
+        console.log("dispari");
+        return "dispari";
+    }
 }
